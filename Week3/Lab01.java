dc2e086b1d7eb13357532a19ab8a3c373102ac4e
Dan Schaper <dschaper@ganymeade.com>
2013-09-10 19:43:48 -0700

    Week 3 lab 01

---
+++ Week3/Lab01.java
@@ -0,0 +1,11 @@
+/**
+ * Date: 9/10/13
+ * Time: 7:40 PM
+ * @author Dan Schaper
+ */
+public class Lab01 {
+    public static void main(String[] args){
+        System.out.print("My name is ");
+        System.out.println("Dan Schaper");
+    }
+}
